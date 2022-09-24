6782077d4f7b0041edaeb13e021da8ac21a2ff84
LYQ <liyaqin_lyq@163.com>
2022-09-24 21:50:19 +0800

    。。。

--- 05索引与切片/main.py
+++ 05索引与切片/main.py
@@ -5,11 +5,11 @@ import torch
 '''
 print('-------------------Indexing-----------------------')
 a = torch.rand(4, 3, 28, 28)  # 想象为一组彩色图片
-print(a[0].shape)   # 第一张图片
+print(a[0].shape)  # 第一张图片
 
-print(a[0, 0].shape)    # 第一张图片的第一个通道
+print(a[0, 0].shape)  # 第一张图片的第一个通道
 
-print(a[0, 0, 2, 4])    # 第一张图片的第一个通道上的第2行第4列的像素，标量
+print(a[0, 0, 2, 4])  # 第一张图片的第一个通道上的第2行第4列的像素，标量
 
 '''
 select first/last N
@@ -29,3 +29,44 @@ print(a[:2, :1, :, :].shape)
 print(a[:2, 1:, :, :].shape)
 
 print(a[:2, -1:, :, :].shape)
+
+'''
+select by steps
+::   两个冒号可设定隔行采样
+'''
+print('-------------------select by steps-----------------------')
+print(a[:, :, 0:28:2, 0:28:2].shape)
+
+print(a[:, :, ::2, ::2].shape)
+
+'''
+...
+'''
+print('-------------------...-----------------------')
+print(a[...].shape)
+print(a[0, ...].shape)  # a[0]
+print(a[:, 1, ...].shape)  # a[:, 1]
+print(a[..., :2].shape)  # a[:, :, :, :]
+
+'''
+select by mask
+掩码
+'''
+x = torch.randn(3, 4)
+print(x)
+
+mask = x.ge(0.5)    # 筛选出所有大于0.5的，生成掩码
+print(mask)
+
+select = torch.masked_select(x, mask)
+print(select, select.shape)
+
+'''
+select by flatten index
+把数据打平后处理，单维度操作
+torch.take()
+'''
+src = torch.tensor([[4, 3, 5],
+                    [6, 7, 8]])
+
+print(torch.take(src, torch.tensor([0, 2, 5])))
